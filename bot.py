10c3694a44adb76b1a31f111bc4a0228a227e22f
Roofiif Ananta Fathan <roofiif2000018419@webmail.uad.ac.id>
2023-07-31 21:19:24 +0700

    tambah neh

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ def start(update: Update, context: CallbackContext):
 def bantuan(update: Update, context: CallbackContext):
 	update.message.reply_text("""
 	Kata Kunci Tersedia :
-	<b>Seputar_ppdb</b> - Berisi informasi umum mengenai PPDB
-	<b>Persyaratan_ppdb</b> - Berisi persyaratan PPDB
+	<b>Seputar ppdb</b> - Berisi informasi umum mengenai PPDB
+	<b>Persyaratan ppdb</b> - Berisi persyaratan PPDB
 	""", parse_mode=ParseMode.HTML)
 
 
@@ -26,9 +26,9 @@ def seputar_ppdb(update: Update, context: CallbackContext):
 	update.message.reply_text("""
 	Seputar PPDB
 	Kata Kunci Tersedia :
-	<b>Jalur_ppdb</b> - Berisi informasi jalur yang tersedia
-	<b>Nilai_prestasi</b> - Berisi informasi Nilai tambahan
-	<b>Daftar_ulang</b> - Berisi informasi Daftar ulang
+	<b>Jalur ppdb</b> - Berisi informasi jalur yang tersedia
+	<b>Nilai prestasi</b> - Berisi informasi Nilai tambahan
+	<b>Daftar ulang</b> - Berisi informasi Daftar ulang
 	""", parse_mode=ParseMode.HTML)
 
 
@@ -36,10 +36,10 @@ def jalur_ppdb(update: Update, context: CallbackContext):
 	update.message.reply_text("""
 	Jalur PPDB
 	Kata Kunci Tersedia :
-	<b>Jalur_zonasi</b> - Berisi informasi jalur Zonasi
-	<b>Jalur_afirmasi</b> - Berisi informasi jalur aformasi
-	<b>Jalur_prestasi</b> - Berisi informasi jalur Prestasi
-	<b>Jalur_pto</b> - Berisi informasi jalur pto
+	<b>Jalur zonasi</b> - Berisi informasi jalur Zonasi
+	<b>Jalur afirmasi</b> - Berisi informasi jalur aformasi
+	<b>Jalur prestasi</b> - Berisi informasi jalur Prestasi
+	<b>Jalur pto</b> - Berisi informasi jalur pto
 	""", parse_mode=ParseMode.HTML)
 
 def daftar_ulang(update: Update, context: CallbackContext):
@@ -69,16 +69,16 @@ def jalur_pto(update: Update, context: CallbackContext):
 def peryaratan_ppdb(update: Update, context: CallbackContext):
 	update.message.reply_text("""
     <b>UMUM</b>
-    - Ijazah/Surat Keterangan Lulus/Kartu peserta Ujian Sekolah
-    - Akta Kelahiran/Surat Keterangan Lahir
-    - Kartu Keluarga (minimal satu tahun), KTP
-    - Buku Rapor (semester 1 s.d. 5)
-    - Surat Tanggung Jawab Mutlak Orang Tua\n\n<b>KHUSUS</b>
-    - Kartu Program Penanganan Kemiskinan/Terdaftar pada DTKS Dinsos (bagi jalur - afirmasi/KETM)
-    - Surat Keterangan Domisili dari RT/RW (bagi afirmasi korban bencana alam/sosial)
-    - Surat Tugas Orangtua (bagi jalur perpindahan tugas orangtua/wali, maks. 3 tahun/anak guru)
-    - Surat keputusan satgas covid bagi afirmasi kondisi tertentu penanganan Covid-19
-    - Piagam dan Dokumentasi Prestasi (untuk jalur prestasi kejuaraan) maks. 5 tahun, min. 6 bulan
+    Ijazah/Surat Keterangan Lulus/Kartu peserta Ujian Sekolah
+    Akta Kelahiran/Surat Keterangan Lahir
+    Kartu Keluarga (minimal satu tahun)
+	Buku Rapor (semester 1 s.d. 5)
+    Surat Tanggung Jawab Mutlak Orang Tua\n\n<b>KHUSUS</b>
+    Kartu Program Penanganan Kemiskinan/Terdaftar pada DTKS Dinsos (bagi jalur - afirmasi/KETM)
+    Surat Keterangan Domisili dari RT/RW (bagi afirmasi korban bencana alam/sosial)
+    Surat Tugas Orangtua (bagi jalur perpindahan tugas orangtua/wali, maks. 3 tahun/anak guru)
+    Surat keputusan satgas covid bagi afirmasi kondisi tertentu penanganan Covid-19
+    Piagam dan Dokumentasi Prestasi (untuk jalur prestasi kejuaraan) maks. 5 tahun, min. 6 bulan
 	""", parse_mode=ParseMode.HTML)
 
 def unknown(update: Update, context: CallbackContext):
@@ -91,11 +91,17 @@ def unknown_text(update: Update, context: CallbackContext):
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^bantuan$'), bantuan))
-updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^persyaratan_ppdb$'), peryaratan_ppdb))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^jalur ppdb$'), jalur_ppdb))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^seputar ppdb$'), seputar_ppdb))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^daftar ulang$'), daftar_ulang))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^nilai prestasi$'), nilai_prestasi))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^jalur zonasi$'), jalur_zonasi))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^jalur afirmasi$'), jalur_afirmasi))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^jalur prestasi$'), jalur_prestasi))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^jalur pto$'), jalur_pto))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'(?i)^persyaratan ppdb$'), peryaratan_ppdb))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(
-	Filters.command, unknown)) # Filters out unknown commands
-
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown)) # Filters out unknown commands
 # Filters out unknown messages.
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
 
